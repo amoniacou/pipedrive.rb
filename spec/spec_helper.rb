11cc4bd654945a165eb4b1a7da3a2c37c0815fac
Bader Sader <bader.bahjat@gmail.com>
2020-10-18 08:56:04 +0300

    fix deprication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'simplecov'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(SimpleCov::Formatter::HTMLFormatter)
 SimpleCov.start do
   add_filter 'spec'
   minimum_coverage(76)
