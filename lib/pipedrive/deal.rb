49a45ea3c27658c577c231a981030c6c6923c837
Xiao Xie <xiao@kamiapp.com>
2022-04-27 16:14:16 +1200

    Add search method to Pipedrive::Deal. Because the struct of search response is so different, cannot apply the follow_pagination method for pagenation

--- lib/pipedrive/deal.rb
+++ lib/pipedrive/deal.rb
@@ -6,5 +6,13 @@ module Pipedrive
     include ::Pipedrive::Operations::Read
     include ::Pipedrive::Operations::Update
     include ::Pipedrive::Operations::Delete
+
+    def search(*args)
+      params = args.extract_options!
+      params[:term] ||= args[0]
+      raise 'term is missing' unless params[:term]
+
+      make_api_call(:get, 'search', params)
+    end
   end
 end
